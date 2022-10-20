8e2aa44ff21af2bb8dd459863a0519e0d3b06b89
Tferdogan <70774541+Tferdogan@users.noreply.github.com>
2022-10-20 20:10:47 +0300

    10/20 Çalışıyor

--- calculator.js
+++ calculator.js
@@ -1,8 +1,50 @@
+
+let screentext = ""
+let lastInputType
+let hasPointAfterOperation = false
 function clicked(id) {
-	alert(id)
-	setScreen(id)
+	let num = Number(id)
+	if (!isNaN(num)){
+		setScreen(screentext + num)
+	}
+	else {
+		let symbol = toSymbolPrint(id)
+		if (symbol)
+			setScreen(screentext + symbol)
+		else {
+			let result = evaluate()
+			setScreen(result)
+		}
+	}
 }
 
 function setScreen(text) {
 	document.getElementById("screentext").value = text;
+	screentext = text
+}
+
+function clearScreen() {
+	setScreen("")
+}
+
+function toSymbolPrint(id){
+	switch(id) {
+		case "point":
+		  return ".";
+		case "plus":
+			return " + ";
+		case "minus":
+			return " - ";
+		case "multiply":
+			return " x ";
+		case "divide":
+			return " / ";
+		default:
+			return null
+	}
+}
+
+function evaluate(){
+	let evaltext = screentext.replace("x", "*")
+	return eval(evaltext)
 }
